55c41ac10b01b62d56b26624af6d187ce7349cc0
Sagar Aher <aher.sagar0@gmail.com>
2022-08-18 19:47:44 +0530

    initial commit manish next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReusableComponent } from './reusable/reusable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReusableComponent
   ],
   imports: [
     BrowserModule,
